70c9ff1c4ad64995c981faf5e8489c168d2539f9
Yasar195 <imyasar07@gmail.com>
2023-02-12 09:55:37 +0530

    some updates

--- socket.js
+++ socket.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const ws = require('ws')
 const wss = new ws.Server({ noServer: true })
 const { v4: uuidv4 } = require('uuid');
-const e = require('express');
 
 const clients = new Set()
 
@@ -23,7 +22,7 @@ wss.people = () => {
     clients.forEach((paraclient)=> {
         const people = []
         clients.forEach((chiclient)=> {
-            if(paraclient.ip === chiclient.ip && paraclient !== chiclient){
+            if(paraclient !== chiclient){
                 people.push(createObject(chiclient))
             }
         })
@@ -51,6 +50,10 @@ wss.Offer = (sender, receiverId, method) => {
     })
 }
 
+wss.close = (ws)=> {
+    ws.close()
+}
+
 wss.left = (sender) => {
     clients.forEach((client)=> {
         client.requests.forEach(req => {
@@ -73,14 +76,23 @@ const onSocketConnection = (ws) => {
 
     clients.add(ws)
 
+    setTimeout(()=> {
+        wss.close(ws)
+    },1000*60)
+
     ws.on('message', (data)=> {
         const request = JSON.parse(data.toString())
         if (request.type == "open"){
             ws.id = uuidv4(),
-            ws.ip = request.ip
             ws.name = request.name
             ws.requests = []
-            wss.people()
+            if(ws.name !== ""){
+                ws.send("success")
+            }
+            else{
+                ws.send("failed")
+            }
+            //wss.people()
         }
         else if(request.type == "offer"){
             wss.Offer(ws, request.id, "offer")
